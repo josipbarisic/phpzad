a584a656a0bd616db6699ca6dc0c023c1dd0fa81
josipbarisic <josip.barisic1998@gmail.com>
2018-03-15 13:47:12 +0100

    Ispravak

--- files/classes/klasa2.php
+++ files/classes/klasa2.php
@@ -7,7 +7,7 @@ class klasa2{
 
     }
 
-    #funkcija1 ispisuje ime klase u kojoj se nalazi, svoje ime, te url 
+    #funkcija ispisuje ime klase u kojoj se nalazi, svoje ime, te url 
     public function funkcija1()
     {
         echo "<p>Ime klase: ".__CLASS__."</p>".PHP_EOL;
